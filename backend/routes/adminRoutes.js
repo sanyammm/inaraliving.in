f40fc23b60f245cff637573c53b186ff87997a26
Sanyam Sharma <yrgrl19sharma@gmail.com>
2025-05-18 00:40:43 +0530

    Rolled back the changes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -11,14 +11,6 @@ router.post("/login", async (req, res) => {
   try {
     const admin = await Admin.findOne({ email });
 
-    console.log("Admin found:", admin);
-    if (!admin) {
-      return res.status(401).json({ message: "Invalid credentials of admin" });
-    }
-    if (!admin || !(await admin.matchPassword(password))) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
     const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
