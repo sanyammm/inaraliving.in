5ef15c4607a5c169b1360d7eca71ac0170c7cabd
Sanyam Sharma <yrgrl19sharma@gmail.com>
2025-07-23 15:07:11 +0530

    updated patch in server.js for CORS

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
